c7c7b15dde8f6e1955f0d961ebc91e039e6c5b5c
Andrey Kolosov <kaa67@narod.ru>
2018-03-25 20:17:16 +0300

    wip

--- src/Kaa/classes/Controller/Index.php
+++ src/Kaa/classes/Controller/Index.php
@@ -3,11 +3,21 @@ namespace Kaa\Controller;
 
 use Core\Controller;
 
-class Index extends Controller\Template
-{
+class Index extends Controller\Template {
+
+    public $template = 'kaa/template';
+
     public function index()
     {
-        $this->template = 'kaa/template';
-        $this->template->content = 'ПРЕВЕД job002';
+        $this->template->navbar = $this->_view('kaa/navbar')
+            ->set('class', 'index');
+        $this->template->content = 'kaa/content';
+    }
+
+    public function about()
+    {
+        $this->template->navbar = $this->_view('kaa/navbar')
+            ->set('class', 'about');
+        $this->template->content = 'kaa/content';
     }
 }
